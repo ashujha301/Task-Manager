8e9f7eb6405ddb8856d4406e1a3ef1a3f367fd71
Ayush Jha <ashujha301@gmail.com>
2024-07-31 01:29:40 +0530

    feat:api endpoints created

--- server/apis/routes/authRoutes.js
+++ server/apis/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const userControllers = require('../controllers/userController');
+const taskController = require('../controllers/taskController');
 
-router.get('/:taskID', userControllers.getSpecificTaskByUser); // 
-router.get('/tasks', userControllers.getAllTaskCreatedByUser); // this route is working fine
+router.post('/', taskController.createTask); //create task
+router.get('/', taskController.getAllTaskCreatedByUser); //get all tasks made by user
+router.get('/:taskID', taskController.getSpecificTaskByUser); //get specific task by user
+router.put('/:taskID', taskController.getSpecificTaskByUser); // update specific task
+router.delete('/:taskID', taskController.getSpecificTaskByUser); // delete specific task
+router.patch('/:taskID/move', taskController.moveTask); //move task drag and drop
 
 
 module.exports = router;
